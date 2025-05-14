c71a75d8463abffcef08a34e7b72659fc521b30e
LUCAS ANASTACIO MEDEIROS DOS SANTOS <lucas.amsantos4@senacsp.edu.br>
2025-05-13 21:03:04 -0300

    alpha 0.2 proto

--- Spring/alpha_0.2/buenosautosagendamento/src/main/java/com/buenosautos/buenosautosagendamento/protocolo/model/Protocolo.java
+++ Spring/alpha_0.2/buenosautosagendamento/src/main/java/com/buenosautos/buenosautosagendamento/protocolo/model/Protocolo.java
@@ -1,5 +1,11 @@
 package com.buenosautos.buenosautosagendamento.protocolo.model;
 
+import com.buenosautos.buenosautosagendamento.solicitacao.model.Solicitacao;
+
 public class Protocolo {
+	private Long id;
+	private Solicitacao solicitacao;
+	
+	
 
 }
---
+++ Spring/alpha_0.2/buenosautosagendamento/src/main/java/com/buenosautos/buenosautosagendamento/protocolo/model/ServicoMecanico.java
@@ -0,0 +1,9 @@
+package com.buenosautos.buenosautosagendamento.protocolo.model;
+
+import com.buenosautos.buenosautosagendamento.solicitacao.model.Servico;
+
+public class ServicoMecanico extends Servico{
+
+	private String status;
+	private String descricao;
+}
